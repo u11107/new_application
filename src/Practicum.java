57b64b276b59ea5dd2149edb321321e9fa5fe4dc
Rinat <eenot84@yandex.ru>
2022-02-13 16:46:09 +0300

    feat: добавить класс Person

--- src/Practicum.java
+++ src/Practicum.java
@@ -64,3 +64,19 @@ public class Practicum {
     }
 
 }
+
+public class Practicum {
+    public static void main(String[] args) {
+        Object anyObject = new Person("Михаил");
+        System.out.println(anyObject);
+
+    }
+}
+
+class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
